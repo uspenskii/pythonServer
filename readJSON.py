bc25add66d102a58bb1b5f65b9aaa3cbc622f6c7
root <san.delli@yandex.ru>
2016-10-06 02:05:39 +0300

    debug menu, search

--- readJSON.py
+++ readJSON.py
@@ -1,7 +1,6 @@
-
 import json
 from urllib.request import urlopen
-# example reading
+
 response = urlopen("http://127.0.0.1:5000/api?action=getGoods").read().decode('utf8')
 obj = json.loads(response)
 for i in  obj['GoodsList']:
--- untitled15.py
+++ untitled15.py
@@ -1,29 +1,28 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template,url_for
 import Database
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():
-    return  render_template('main.html')
+    return render_template('main.html')
 
 
-@app.route('/api',methods=['GET'])
+@app.route('/api', methods=['GET'])
 def test():
     print(request.args)
     if request.args['action'] == "getGoods":
-        data = Database.getGoods()
-        return data
+        return Database.getGoods(request.args['type'])
 
+    if request.args['action'] == "search":
+        return Database.search(request.args['request'])
 
     return "Error"
 
 if __name__ == '__main__':
-    for i in range(255):
+    for i in range(1, 255):
         try:
-            app.run(host="192.168.0.{0}".format(i))
+            app.run(host="192.168.0.{0}".format(i),)
         except Exception:
             pass
-
